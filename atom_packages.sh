963235dc925cc7e910f2da62e51ec4d6c898f068
Evandro Junior <evandrojr@gmail.com>
2015-11-11 15:38:08 -0300

    new file:   atom_packages.sh

---
+++ atom_packages.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+#apm install language-ruby
+apm install ruby-test-switcher
+apm install tabs-to-spaces
+apm install atom-ctags
+#apm install autocomplete-plus
+apm install ruby-block
+apm install atom-rails
+apm install file-icons
+apm install git-plus
+apm install remote-edit
+apm install ruby-test
+apm install execute-as-ruby
+

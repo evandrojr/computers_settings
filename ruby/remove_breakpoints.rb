b51c77fb4b598406c48a071c409de513e34b88f8
Evandro Junior <evandrojr@gmail.com>
2015-11-11 17:01:45 -0300

    new file:   ruby/remove_breakpoints.rb

---
+++ ruby/remove_breakpoints.rb
@@ -0,0 +1,46 @@
+#!/usr/bin/env ruby
+require 'fileutils'
+
+all_files = Dir.glob("**/*").select {|f| !File.directory?(f) and ( f.end_with?(".rb")  or f.end_with?(".erb") )  }
+files_with_bp = []
+
+all_files.each do |filename|
+  begin
+    open(filename, 'r') do |f|
+      f.each_line do |line|
+        if /.*binding\.pry.*/ =~ line
+          files_with_bp << filename
+          break
+        end
+    end
+      end
+  rescue => error
+    puts("#{error.class} and #{error.message}")
+  end
+end
+
+puts "Files with breakpoint"
+puts files_with_bp.inspect
+
+files_with_bp.each do |filename|
+  put filename if filename == 'doc/aaa'
+  problem_writing_file = false
+  open(filename, 'r') do |f|
+    break if problem_writing_file
+    open("#{filename}.rb_tmp", 'w') do |f2|
+      f.each_line do |line|
+        begin
+         f2.write(line) unless /.*binding\.pry.*/ =~ line
+        rescue
+          problem_writing_file = true
+          break
+        end
+      end
+    end
+  end
+  unless problem_writing_file
+    s = File.stat(filename)
+    FileUtils.mv "#{filename}.rb_tmp", filename
+    File.chmod(s.mode, filename)   #=> 1
+  end
+end

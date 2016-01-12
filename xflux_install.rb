e7892abbccb02b76a2930659fbeb2b1203c9deaf
Evandro Junior <evandrojr@gmail.com>
2016-01-12 16:58:06 -0300

    fix xflux instal

--- xflux_install.rb
+++ xflux_install.rb
@@ -5,8 +5,8 @@ include FileUtils
 
 if RUBY_PLATFORM.start_with?("x86_64")
   puts "64bits"
-  cp  "xflux/64bits/xflux", "~bin"
+  `cp  xflux/64bits/xflux ~/bin`
 else
   puts "32bits"
-  cp "xflux/64bits/xflux", "~bin"
+  `cp  xflux/32bits/xflux ~/bin`
 end

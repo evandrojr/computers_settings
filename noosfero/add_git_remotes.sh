87bf628cd4f6955210c0e79cd8949a82daf25f7e
Evandro Junior <evandrojr@gmail.com>
2016-04-22 15:09:13 -0300

    added remotes and improved pha

---
+++ noosfero/add_git_remotes.sh
@@ -0,0 +1,6 @@
+git remote add community	git@gitlab.com:noosfero/noosfero.git
+git remote add github-evandrojr-noosfero	git@github.com:evandrojr/noosfero.git
+git remote add github-evandrojr-noosferogov	git@github.com:evandrojr/noosferogov.git
+git remote add gitlab-evandrojr-noosfero	git@gitlab.com:evandrojr/noosfero.git
+git remote add gitlab-participa	git@gitlab.com:participa/noosfero.git
+git remote add origin	git@softwarepublico.gov.br:noosferogov/noosfero.git

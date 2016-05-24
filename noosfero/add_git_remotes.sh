2ace385563a2350bb8c2fc474c3665b403b09c6d
Evandro Junior <evandrojr@gmail.com>
2016-05-24 10:15:32 -0300

    add remote and fix alias

--- noosfero/add_git_remotes.sh
+++ noosfero/add_git_remotes.sh
@@ -1,6 +1,7 @@
-git remote add community	git@gitlab.com:noosfero/noosfero.git
-git remote add github-evandrojr-noosfero	git@github.com:evandrojr/noosfero.git
-git remote add github-evandrojr-noosferogov	git@github.com:evandrojr/noosferogov.git
-git remote add gitlab-evandrojr-noosfero	git@gitlab.com:evandrojr/noosfero.git
-git remote add gitlab-participa	git@gitlab.com:participa/noosfero.git
-git remote add origin	git@softwarepublico.gov.br:noosferogov/noosfero.git
+git remote add community git@gitlab.com:noosfero/noosfero.git
+git remote add github-evandrojr-noosfero git@github.com:evandrojr/noosfero.git
+git remote add github-evandrojr-noosferogov git@github.com:evandrojr/noosferogov.git
+git remote add gitlab-evandrojr-noosfero git@gitlab.com:evandrojr/noosfero.git
+git remote add gitlab-participa git@gitlab.com:participa/noosfero.git
+git remote add origin git@softwarepublico.gov.br:noosferogov/noosfero.git
+git remote add spb git@softwarepublico.gov.br:noosferogov/noosfero.git

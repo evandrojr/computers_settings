c7d797147e0ec72f5be33f61d3a46ced1f6c6ad3
Evandro Junior <evandrojr@gmail.com>
2016-07-09 05:45:34 -0300

    fix nvm_setup

--- nvm_setup.sh
+++ nvm_setup.sh
@@ -2,22 +2,11 @@
 
 SRC=$(cd $(dirname "$0"); pwd)
 source "${SRC}/core.sh"
-RUBY_VERSION = "2.3.0"
+NODE_VERSION = "6.3.0"
 
+sudo apt-get install curl
 run curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.1/install.sh | bash
 
-run rm -rf ~/.rbenv
-run git clone https://github.com/rbenv/rbenv.git ~/.rbenv
-run cd ~/.rbenv && src/configure && make -C src
-run echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
-run echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
-run git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
-run rbenv install $RUBY_VERSION
-run rbenv rehash
-rbenv global $RUBY_VERSION
-gem install bundler
-run git clone -- https://github.com/carsomyr/rbenv-bundler.git  ~/.rbenv/plugins/bundler
-say "Turning on auto bundle exec"
-run rbenv bundler on
-run rbenv install -l
-say "Now rbenv install your favourite version"
+run nvm install $NODE_VERSION
+
+say "Done!"

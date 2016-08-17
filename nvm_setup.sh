82372961db09392e1dd7eaf8586d933d15a83b6a
Evandro Junior <evandrojr@gmail.com>
2016-08-17 06:12:54 -0300

    corrigido o NODE_VERSION

--- nvm_setup.sh
+++ nvm_setup.sh
@@ -2,7 +2,7 @@
 
 SRC=$(cd $(dirname "$0"); pwd)
 source "${SRC}/core.sh"
-NODE_VERSION = "6.3.0"
+NODE_VERSION="6.3.0"
 
 sudo apt-get install curl
 run curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.1/install.sh | bash
